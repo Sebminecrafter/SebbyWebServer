d37d1f2de1871a84711f79a733b12d1ac2f5ee52
Sebminecrafter <116523841+Sebminecrafter@users.noreply.github.com>
2025-09-01 09:57:08 -0400

    update code to work better with new python

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def evalRequest(path):
             status = 200
             if str(mimetypes.guess_file_type(path2)[0]) == 'text/html':
                 with open(path2, 'r') as f:
+                    output = f.read()
                     output = f"{output}{append}".encode('UTF-8')
                 type = 'text/html'
             else:
@@ -40,7 +41,8 @@ def evalRequest(path):
                 output = f"{output}{append}".encode('UTF-8')
                 type = 'text/html'
         else:
-            output = notfoundpage.encode('UTF-8')
+            cr404page = notfoundpage.replace("PATH",path)
+            output = cr404page.encode('UTF-8')
             status = 404
             type = 'text/html'
     return output,status,type
@@ -53,41 +55,40 @@ class SebbyServer(BaseHTTPRequestHandler): #just runs another function -_-
         self.wfile.write(response[0])
 
 
-port = 80; host = ''; keyfilelocation = ''; certfilelocation = ''; sebby_using_ssl = False; handler_class=SebbyServer; append = f"\n<br><center><p>Running SebbyWebServer Version {strver}</p></center>"; notfoundpage = f"<h1>404 File/Function not found: {path}</h1>"
+port = 80; host = ''; keyfilelocation = ''; certfilelocation = ''; sebby_using_ssl = False; handler_class=SebbyServer; append = f"\n<br><center><p>Running SebbyWebServer Version {strver}</p></center>"; notfoundpage = "<h1>404 File/Function not found: PATH</h1>"
 #Load config file
 try:
     with open('config.txt', 'r') as config:
-        configlines = config.readlines()
-        for i in configlines:
-            eval(i) #its lit just python T-T, be careful bro
+        for i in config.readlines():
+            exec(i)
         config.close() #close file cuz yes
 except Exception:
     print("Failed to load config file, making you a new one")
-    #bro forgor he deleted the config, so make him a new one
+    #make a new config if one does not exist
     with open('config.txt', 'w') as config:
-        config.write("""#NOTICE: THIS WILL BE INTERPRETED AS LITERAL PYTHON CODE (because why not) SO BE CAREFUL AND READ ANYTHING BEFORE YOU PUT IT IN HERE
-# :)                                --------------------------------Config--------------------------------
-port = 80                                                                        #Which port to serve on (default for http: 80, https: 443)
-host = ''                                                                        #Hostname/ip change if you aren't just devtesting
-keyfilelocation = ''                                                             #Only required for ssl, location of your key file (private.key)
-certfilelocation = ''                                                            #Only required for ssl, location of your certificate (cert.pem)
-sebby_using_ssl = False                                                          #Whether you are using SSL (HTTPS) or not (HTTP)
-handler_class = SebbyServer                                                      #Only change if you know are testing/editing/etc. ()
-append = f"\n<br><center><p>Running SebbyWebServer Version {strver}</p></center>"#Appended at end of page
-notfoundpage = f"<h1>404 File/Function not found: {path}</h1>"                   #File not found (404) page""")
-        config.close() #close file cuz yes
+        config.write("""#NOTICE: THIS WILL BE INTERPRETED AS CODE, BE CAREFUL AND READ ANYTHING BEFORE YOU PUT IT IN HERE
+#                                                                                 --------------------------------Config--------------------------------
+port = 443                                                                        #Which port to serve on (default for http is 80, https is 443)
+host = ''                                                                         #Hostname/ip change if you aren't just devtesting
+keyfilelocation = 'cert/private.key'                                              #Only required for ssl, location of your key file (private.key)
+certfilelocation = 'cert/cert.pem'                                                #Only required for ssl, location of your certificate (cert.pem)
+sebby_using_ssl = True                                                            #Whether you are using SSL (HTTPS) or not (HTTP)
+handler_class=SebbyServer                                                         #Only change if you are testing/editing/etc.
+append = f"\\n<br><center><p>Running SebbyWebServer Version {strver}</p></center>"#Appended at end of page
+notfoundpage = "<h1>404 File/Function not found: PATH</h1>"                       #File not found (404) page you can use PATH for the path""")
+        config.close() #close file
 
 if __name__ == '__main__':
     server_address = (host, port)
     httpd = ThreadingHTTPServer(server_address, handler_class)
-    if seeby_using_ssl:
+    if sebby_using_ssl:
         sslctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         sslctx.check_hostname = False
         sslctx.load_cert_chain(certfile=certfilelocation, keyfile=keyfilelocation)
         httpd.socket = sslctx.wrap_socket(httpd.socket, server_side=True)
-        print("HTTPS is enabled! :)") #les goo https!!
+        print("HTTPS is enabled")
     else:
-        print("Running in HTTP mode. :/") #meh its ok but you have to manually type it in nowadays
+        print("HTTPS is disabled")
     if server_address[0] == '': printserver = '127.0.0.1 or localhost'
     else: printserver = server_address[0]
     print(f"SebbyWebServer\nServing on {printserver}:{server_address[1]}\nPress Ctrl+C to stop") #gotta tell chat how it works bro
@@ -96,5 +97,5 @@ if __name__ == '__main__':
     except KeyboardInterrupt: #stop server when Ctrl+C pressed
         pass
     print("Stopping server...")
-    httpd.server_close() #close it properly man
-    print("Stopped.") #It's now safe to turn off your computer (ain't nobody gonna get this reference)
+    httpd.server_close() #close the server
+    print("Stopped.")
